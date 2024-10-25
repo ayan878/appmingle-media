2664507c3c8570affe3502d8ecfa537e0b01387d
ayan878 <ayanraza197@gmail.com>
2024-10-25 19:30:12 +0530

    expert team

--- frontend/src/components/Expertteam.jsx
+++ frontend/src/components/Expertteam.jsx
@@ -14,7 +14,7 @@ const pics = [
 
 function Expertteam() {
   return (
-    <div className="mx-auto bg-white py-12 px-20">
+    <div className="mx-auto bg-white py-12 px-4 md:px-20">
       <img src={wave} alt="wave" className="h-24 w-40 mx-auto" />
       <h1 className="text-3xl font-bold text-center font-mono mb-2">
         Our Expert Team
@@ -24,18 +24,24 @@ function Expertteam() {
         innovative solutions, delivering exceptional results with dedication and
         collaboration.
       </p>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-16 md:gap-2">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6">
         {pics.map((pic, index) => (
           <div
             key={index}
-            className="relative w-56 flex flex-col items-center justify-between shadow-md overflow-hidden "
+            className="relative flex flex-col items-center justify-between shadow-md overflow-hidden w-full max-w-xs mx-auto"
           >
-            <img src={pic.src} alt={pic.desc} className="h-52 w-full mb-2" />
+            <img
+              src={pic.src}
+              alt={pic.desc}
+              className="h-52 w-full mb-2 object-cover"
+            />
             <span className="text-center font-semibold">{pic.desc}</span>
-            <span className="text-center font-semibold mb-4">{pic.designation}</span>
+            <span className="text-center font-semibold mb-4">
+              {pic.designation}
+            </span>
             <FaLinkedin className="absolute top-4 left-4 text-blue-600" />
             <button className="absolute bottom-4 right-4 transform translate-x-1/2 translate-y-1/2 font-bold w-16 h-16 text-white rounded-full bg-gray-200">
-              <FaPlus className="ml-5 -mt-1 text-blue-500"/>
+              <FaPlus className="ml-5 -mt-1 text-blue-500" />
             </button>
           </div>
         ))}
