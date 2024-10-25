f0f09ed83e290007ae592715b47de5351f8f6e62
ayan878 <ayanraza197@gmail.com>
2024-10-25 18:29:45 +0530

    blog

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
         {/* Sidebar */}
         <button
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-800 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-expanded={openMenu}
           onClick={() => setOpenMenu(!openMenu)}
         >
