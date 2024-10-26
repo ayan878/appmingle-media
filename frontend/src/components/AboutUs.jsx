1e58bb6c2cc8f0a2a7012e888793b1fdec14ea09
ayan878 <ayanraza197@gmail.com>
2024-10-26 14:47:39 +0530

    home

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,51 +1,104 @@
 import wave from "../assets/wave1.png";
-import Sriram from "../assets/Untitled-design-91.png";
-import Abhay from "../assets/Untitled-design-90.png";
-import Raghav from "../assets/Untitled-design-87.png";
-import Hareram from "../assets/Untitled-design-86.png";
-import { FaLinkedin, FaPlus } from "react-icons/fa";
-
-const pics = [
-  { src: Sriram, desc: "Sriram Singh", designation: "CEO & FOUNDER" },
-  { src: Abhay, desc: "Abhay Singh", designation: "CTO" },
-  { src: Raghav, desc: "Raghav", designation: "CTO" },
-  { src: Hareram, desc: "Hareram Singh", designation: "CTO" },
-];
+import { FaCheck } from "react-icons/fa";
+import { IoMdCall } from "react-icons/io";
+import image1 from "../assets/AppMingle-Media.jpg";
+import image2 from "../assets/Untitled-design-76.png";
 
 function AboutUs() {
   return (
-    <div className="mx-auto bg-white py-12 px-4 md:px-20">
-      <img src={wave} alt="wave" className="h-24 w-40" />
-      <h1 className="text-3xl font-bold text-start font-mono mb-2">
-        About Us
-      </h1>
-      <p className="text-start font-serif mb-4">
-        At AppMingle Media, we are passionate about creating innovative digital
-        solutions that drive business growth and enhance user experiences. Our
-        dedicated team of 20 talented professionals brings a wealth of expertise
-        in cutting-edge technologies to every project we undertake.
-      </p>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6">
-        {pics.map((pic, index) => (
-          <div
-            key={index}
-            className="relative flex flex-col items-center justify-between shadow-md overflow-hidden w-full max-w-xs mx-auto"
-          >
-            <img
-              src={pic.src}
-              alt={pic.desc}
-              className="h-52 w-full mb-2 object-cover"
-            />
-            <span className="text-center font-semibold">{pic.desc}</span>
-            <span className="text-center font-semibold mb-4">
-              {pic.designation}
-            </span>
-            <FaLinkedin className="absolute top-4 left-4 text-blue-600" />
-            <button className="absolute bottom-4 right-4 transform translate-x-1/2 translate-y-1/2 font-bold w-16 h-16 text-white rounded-full bg-gray-200">
-              <FaPlus className="ml-5 -mt-1 text-blue-500" />
-            </button>
+    <div className="flex flex-col md:flex-row mt-8 mx-8">
+      <div className="bg-white py-12 px-4 md:px-20 lg:max-w-4xl">
+        <img
+          src={wave}
+          alt="wave"
+          className="mx-auto md:ml-0 h-24 w-28 md:w-40"
+        />
+        <h1 className="text-3xl text-center font-bold md:text-start font-mono mb-4">
+          About Us
+        </h1>
+        <p className="text-center md:text-start font-serif mb-4 text-gray-700">
+          At AppMingle Media, we are passionate about creating innovative
+          digital solutions that drive business growth and enhance user
+          experiences. Our dedicated team of 20 talented professionals brings a
+          wealth of expertise in cutting-edge technologies to every project we
+          undertake.
+        </p>
+        <div className="flex md:flex-row bg-zinc-100 gap-8 px-6 rounded-md">
+          <div className="flex flex-col items-center justify-center md:border-b-0 border-r md:pb-0 pb-4 md:pr-4">
+            <h1 className="text-5xl font-bold font-serif text-blue-400 py-2 px-8">
+              20+
+            </h1>
+            <p className="text-xs font-semibold">DEVELOPMENT TEAM</p>
           </div>
-        ))}
+          <div>
+            <ul className="ml-4 text-xl font-serif py-4 text-gray-700">
+              <li className="flex items-center gap-2">
+                <FaCheck className="text-blue-500" />
+                Web Development
+              </li>
+              <li className="flex items-center gap-2">
+                <FaCheck className="text-blue-500" />
+                Mobile App Development
+              </li>
+              <li className="flex items-center gap-2">
+                <FaCheck className="text-blue-500" />
+                Digital Marketing
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex items-center justify-between mt-6">
+          <a
+            href="tel:+916203689042"
+            className="bg-blue-400 text-white py-2 px-6 mt-4 hover:bg-blue-500 transition"
+          >
+            GET CALL BACK
+          </a>
+          <div className="flex items-center gap-2 mt-4 mr-40 md:mt-0">
+            <p className="p-2 border rounded-full">
+              <IoMdCall className="text-blue-400 w-8 h-8" />
+            </p>
+
+            <span>
+              <p className="font-thin font-serif text-zinc-500">
+                CONTACT US NOW
+              </p>
+              <p className="text-md font-serif font-bold">+91 6203689042</p>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="relative">
+          <img src={image1} className="w-full md:w-64 mb-60 sm:mb-0" />
+          <div className="absolute top-[500px] left-28 md:top-0 md:left-1/2 z-30 bg-blue-500 text-white p-6 justify-between border-8 md:border-r-0 md:border-t-0 border-white">
+            <div className="space-y-6">
+              <div className="space-y-2">
+                <h3 className="text-lg font-semibold flex items-center">
+                  <span className="mr-2">👥</span> Expert Team
+                </h3>
+                <p className="text-sm">
+                  Skilled, experienced, dedicated, innovative.
+                </p>
+              </div>
+              <div className="space-y-2">
+                <h3 className="text-lg font-semibold flex items-center">
+                  <span className="mr-2">⏰</span> 24/7 Support
+                </h3>
+                <p className="text-sm">
+                  Constant, reliable, responsive assistance.
+                </p>
+              </div>
+              <div className="space-y-2">
+                <h3 className="text-lg font-semibold flex items-center">
+                  <span className="mr-2">💼</span> Free Consult
+                </h3>
+                <p className="text-sm">Complimentary, no-cost expert advice.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src={image2} />
       </div>
     </div>
   );
