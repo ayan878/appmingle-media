2de2758a9f32d498c8b677747071aca31c8c3b70
ayan878 <ayanraza197@gmail.com>
2024-10-27 17:34:12 +0530

    fix navbar new line

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -46,29 +46,28 @@ function Header() {
           className={`w-full md:block md:w-auto ${openMenu ? "" : "hidden"}`}
         >
           <ul className="flex flex-col items-start md:items-center bg-white md:bg-transparent font-medium mt-4 md:space-x-8 md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 border">
-            <li className="w-full">
+            <li className="">
               <Link
                 to="/"
-                className="block py-2 px-3 text-black md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent border-b md:border-none"
+                className="block py-2 px-3 text-black md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                 aria-current="page"
               >
                 HOME
               </Link>
             </li>
-            <li className="w-full">
+            <li className="">
               <Link
                 to="about-us"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent border-b md:border-none"
+                className="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 ABOUT US
               </Link>
             </li>
-            <li className="w-full">
+            <li className="">
               <div className="relative">
                 <button
                   onMouseEnter={() => setOpenDropdown(!openDropdown)}
-                  onMouseLeave={() => setOpenDropdown(!openDropdown)}
-                  className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent border-b md:border-none"
+                  className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                 >
                   SERVICES
                   <IoMdArrowDropdown className="hover:transition-all hover:rotate-180" />
@@ -130,23 +129,23 @@ function Header() {
               </div>
             </li>
 
-            <li className="w-full">
+            <li className="">
               <Link
                 to="/blog"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent border-b md:border-none"
+                className="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 BLOG
               </Link>
             </li>
-            <li className="w-full">
+            <li className="">
               <Link
                 to="/contact-us"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent border-b md:border-none"
+                className="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 CONTACT US
               </Link>
             </li>
-            <li className="w-full">
+            <li className="">
               <Link
                 to="/"
                 className="hidden md:flex ml-4 py-2 px-2 bg-blue-400 text-gray-900 hover:bg-gray-100 md:hover:bg-blue-300 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
