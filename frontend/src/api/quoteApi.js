22c7f0efbcf0a1592d6c2b79ae84e32b1972995c
ayan878 <ayanraza197@gmail.com>
2024-10-29 02:59:55 +0530

    ip add for check data save from android

--- frontend/src/api/quoteApi.js
+++ frontend/src/api/quoteApi.js
@@ -1,6 +1,6 @@
 const quote = async (formData) => {
   try {
-    const response = await fetch("http://192.168.0.106:3000", {
+    const response = await fetch("http://localhost:3000", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
