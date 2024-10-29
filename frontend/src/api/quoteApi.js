afd5ad8fea7e5cbcbd90174286b1033c68acfa3a
ayan878 <ayanraza197@gmail.com>
2024-10-29 20:14:05 +0530

    production ready app

--- frontend/src/api/quoteApi.js
+++ frontend/src/api/quoteApi.js
@@ -1,15 +1,17 @@
-const API_URL =
-  import.meta.env.VITE_API_URL || "https://appmingle-media-backend.vercel.app";
+// const API_URL = import.meta.env.VITE_API_URL || "https://appmingle-media-backend.vercel.app";
 
 const quote = async (formData) => {
   try {
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+    const response = await fetch(
+      "https://appmingle-media-backend.vercel.app",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
