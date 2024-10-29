4b1e70d46bece699361c5c146ae38a11d477eb06
ayan878 <ayanraza197@gmail.com>
2024-10-29 19:28:39 +0530

    production ready app

--- backend/src/Router.js
+++ backend/src/Router.js
@@ -3,14 +3,14 @@ import quotes from "./controllers/quoteController.js";
 
 const router = express.Router();
 
-router.post("/test", quotes);
+router.post("/", quotes);
 
 router.get("/", (req, res) => {
   res.status(200).json("Welcome, your app is working well");
 });
 
-// router.post("/test", (req, res) => {
-//   res.status(200).json({ success: true, message: "Test successful!" });
-// });
+router.post("/test", (req, res) => {
+  res.status(200).json({ success: true, message: "Test successful!" });
+});
 
 export default router;
