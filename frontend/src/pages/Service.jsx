dc7a173bf7094c2d0283377cbfe8d2e36520efbf
ayan878 <ayanraza197@gmail.com>
2024-10-30 01:16:06 +0530

    login

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 function Service() {
   return (
-    <div>Service</div>
-  )
+    <div>
+      <Header />
+      Service
+      <Footer />
+    </div>
+  );
 }
 
-export default Service
+export default Service;
